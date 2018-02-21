b16d3f022ac32e9907573774d3569c75803ce347
David Armstrong <davidarmstrong@Davids-MacBook-Pro.local>
2018-02-21 11:24:05 +0000

    DJA updates

--- funcs.py
+++ funcs.py
@@ -9,19 +9,19 @@ class CBSystem(object):
 
     def __init__(self,m1 = None, f1 = None, m2 = None, f2 = None,
                       ab = None, r1 = None, r2 = None, eb = None,
-                      ib = np.pi/2., wb = None, fb = None,
+                      ib = np.pi/2., wb = None, fb = None, Wb = 0.0,
                       ld11 = 0.85, ld21 = -0.09, ld12 = 0.85, ld22 = -0.09,
                       mp = 0.0 , ap = None, rp = None, ep = 0.0,
-                      ip = np.pi/2., wp = None, fp = None, t0 = 0.0):
+                      ip = np.pi/2., wp = None, fp = None, Wp = 0.0, t0 = 0.0):
         '''Initialisation.
             
         Parameters
         ----------
         m1, m2, mp : float
             Mass of binary components and planet, in Solar masses.
-        ab, eb, ib, fb, wb : float
+        ab, eb, ib, fb, wb, Wb : float
             Orbital elements of binary, ab in au and radians for angles.
-        ap, ep, ip, fp, wp : float
+        ap, ep, ip, fp, wp, Wp : float
             Orbital elements of planet, ap in au and radians for angles.
         fl1, fl2 : float
             Fractional flux of binary components, planet is non-luminous.
@@ -44,6 +44,7 @@ class CBSystem(object):
         self.ib = ib
         self.wb = wb
         self.fb = fb
+        self.Wb = Wb
         self.ld11 = ld11
         self.ld21 = ld21
         self.ld12 = ld12
@@ -55,6 +56,7 @@ class CBSystem(object):
         self.ip = ip
         self.wp = wp
         self.fp = fp
+        self.Wp = Wp
         self.t0 = t0
 
 
@@ -122,11 +124,11 @@ def reb_cb(cb,tmin=None,tmax=None):
     sim.t = 0.0
     sim.add(m = cb.m1)
     comp = rebound.Particle(simulation=sim, m=cb.m2, a=cb.ab, e=cb.eb,
-                            inc=cb.ib, omega=cb.wb, f=cb.fb, Omega = 0.0)
+                            inc=cb.ib, omega=cb.wb, f=cb.fb, Omega=cb.Wb)
     sim.add(comp)
     sim.move_to_com()
     planet = rebound.Particle(simulation=sim, m=cb.mp, a=cb.ap, e=cb.ep,
-                              inc=cb.ip, omega=cb.wp, f=cb.fp, Omega=0.0)
+                              inc=cb.ip, omega=cb.wp, f=cb.fp, Omega=cb.Wp)
     sim.add(planet)
     sim.move_to_com()
 
@@ -157,7 +159,7 @@ def reb_cb(cb,tmin=None,tmax=None):
 
 
 def reb_cb_c(cb, tmin=None, tmax=None,
-             cb_path='/Users/grant/astro/code/github/rebound/examples/circumbinary'):
+             cb_path='/Users/davidarmstrong/Software/git-repos/rebound/examples/circumbinary'):
     '''As reb_cb, but using compiled c binary.'''
 
     # set times relative to tmin to avoid precision loss
@@ -187,6 +189,112 @@ def reb_cb_c(cb, tmin=None, tmax=None,
 
     return tts
 
+def reb_cb_dvm(cb, baseBody, transitingBody, tmin, tmax, timing_precision):
+    '''Return transit times for a circumbinary system.
+    
+    Parameters
+    ----------
+    cb : CBSys object
+        Class holding system configuration.
+    baseBody, transitingBody : int
+    	Which transits to extract. Use 0, 1, 2 for primary star, secondary star, planet.
+    tmin, tmax : float
+        Start and end times of simulation, in days.
+    timing_precision : float
+    	Desired precision on transit times, in years/2pi (yes the units need updating).
+    '''
+    # Load some initial stuff
+    transittimes = []
+    transitdurations = []
+
+    # create the rebound simulation and set the units
+	
+    sim = rebound.Simulation()
+		
+    # put the radii into an array, to be used later for transit calculations
+    R = [cb.r1,cb.r2,cb.rp]
+	
+    # add the three bodies to rebound
+    sim.t = 0.0
+    sim.add(m = cb.m1)
+    comp = rebound.Particle(simulation=sim, m=cb.m2, a=cb.ab, e=cb.eb,
+                            inc=cb.ib, omega=cb.wb, f=cb.fb, Omega=cb.Wb)
+    sim.add(comp)
+    sim.move_to_com()
+    planet = rebound.Particle(simulation=sim, m=cb.mp, a=cb.ap, e=cb.ep,
+                              inc=cb.ip, omega=cb.wp, f=cb.fp, Omega=cb.Wp)
+    sim.add(planet)
+
+    # below you can set the number of active particles
+    # active particle means it has mass and you therefore have to calculate its gravitational force
+    # on other bodies. 3=all, 2=massless planet
+    sim.N_active = 3 
+
+    # put everything to the centre of mass	
+    sim.move_to_com()
+
+    # integrate to start time
+    sim.integrate(tmax = (tmin - cb.t0)/365.25 * 2 * np.pi )
+	
+    # p is a reference to the positions and velocities of all three bodies
+    p = sim.particles
+	
+    # Get a reference to the period of the transiting body, used for integration timing
+    if (transitingBody == 0 or transitingBody == 1):
+        P_bin = (cb.ab**3/(cb.m1+cb.m2))**(1./2.)/(2*np.pi) #in years/2pi
+        P_transiter = P_bin
+    elif (transitingBody == 2):
+        P_p = p_p0 = (cb.ap**3/(cb.m1+cb.m2))**(1./2.)/(2*np.pi) #in years/2pi
+        P_transiter = P_p
+		
+    # Integrate the system from time 0 to tMax
+    while sim.t<(tmax - cb.t0)/365.25 * 2 * np.pi :
+        # The old x position of the transiting body with respect to the base body
+        x_old = p[transitingBody].x - p[baseBody].x 
+        # and the corresponding time
+        t_old = sim.t
+        # Integrate over a quarter of the planet's period
+        sim.integrate(sim.t + P_transiter/4.) 
+        # Calculate a new position and time
+        x_new = p[transitingBody].x - p[baseBody].x # old x position of the transiting body with respect to the base body
+        t_new = sim.t
+        # Check if the sign has changed on the x axis (a crossing) and the planet is in front of the star (z<0)
+        # Remember that as an observer we are looking down the positive z-axis
+        if x_old*x_new<0. and (p[transitingBody].z - p[baseBody].z) > 0: 
+            while t_new-t_old>timing_precision:	# do a bisection to a set precision
+                if x_old*x_new<0.:
+                    t_new = sim.t
+                else:
+                    t_old = sim.t
+                sim.integrate((t_new+t_old)/2.)
+                x_new = p[transitingBody].x - p[baseBody].x 
+            # finished the bisection because the t_old and t_new are now within the precision
+            # now we can store the time
+            transittimes.append(sim.t) 
+            # Now want to calculate the transit duration
+            # Calculate impact parameter over the star being transited
+            # Calculated just by looking at the y parameter of the planet with respect to the star
+            bi = np.abs(p[baseBody].y - p[transitingBody].y)/R[baseBody] 
+            if bi < 1: # a transit occurred because the impact parameter is less than 1
+                # the transit duration is just a simple distance/speed calculation
+                transitdurations.append(2*((R[baseBody]+R[transitingBody])**2. - (bi*R[baseBody])**2.)**(1./2.)/((p[transitingBody].vx-p[baseBody].vx)**2. + (p[transitingBody].vy-p[baseBody].vy)**2.)**(1./2.))
+            else: # no transit occured
+                transitdurations.append(0)
+            # Note that the transit time is always stored, regardless of whether or not a transit actually occurs (impact parameter < 1)
+            # The transit time is purely due to a crossing of the x-axis
+            # A transit duration of 0 indicates that the planet missed the star.
+            sim.integrate(sim.t + P_transiter/10.) # add a 10th of the planet's orbital period just to push past the transit
+
+    # get transit times and convert back to days
+    tts = np.array(transittimes)
+    tds = np.array(transitdurations)
+
+    tts /= 2.0 * np.pi / 365.25
+    tts += cb.t0
+    if np.sum(tds>0)>0:
+        tds /= 2.0 * np.pi / 365.25
+    return tts, tds
+
 
 def convfm(f_in,ecc):
     '''Convert true anomaly to mean anomaly.
@@ -204,9 +312,26 @@ def convfm(f_in,ecc):
     bigm = bige - ecc*np.sin(bige)
     return bigm
 
+def convlamb2f(lamb_in, w, ecc):
+    '''Convert mean longitude to true anomaly.
+    
+    Parameters
+    ----------
+    lamb_in : float
+        Mean longitude, in radians.
+    w : float
+    	Argument of periastron, in radians.
+    ecc : float
+        Eccentricity.
+    '''
+    from orbital import utilities
+    
+    bigm = lamb_in - w
+    fb = utilities.true_anomaly_from_mean(ecc, bigm)
+    return fb
 
 def pd_cb(cb, times=None, filed=None, filet=None, cleanup=True,
-          run='/Users/grant/Dropbox/astro/code/github/photodynam/photodynam'):
+          run='/Users/davidarmstrong/Software/git-repos/photodynam/photodynam'):
     '''Return flux from photodynam for a circumbinary system.
     
     Parameters
@@ -241,9 +366,9 @@ def pd_cb(cb, times=None, filed=None, filet=None, cleanup=True,
         f.write('{} {} {}\n'.format(cb.ld21, cb.ld22, 0.0))
         # orbital elements, a, e, i, w, O=0.0, M
         f.write('{} {} {} {} 0.0 {}\n'.format(cb.ab, cb.eb, cb.ib, cb.wb,
-                                              convfm( cb.fb, cb.eb )))
-        f.write('{} {} {} {} 0.0 {}\n'.format(cb.ap, cb.ep, cb.ip, cb.wp,
-                                              convfm( cb.fp, cb.ep )))
+                                              np.mod(convfm( cb.fb, cb.eb ),2*np.pi)))
+        f.write('{} {} {} {} {} {}\n'.format(cb.ap, cb.ep, cb.ip, cb.wp, cb.Wp,
+                                              np.mod(convfm( cb.fp, cb.ep ),2*np.pi)))
                  
     # write the times file, we want just flux as we know the times
     with open(filet,'w') as f:
@@ -290,7 +415,69 @@ def stack(t, f, cb, window=1, event=20):
 
     return dates, stack
 
+def running_mean(x, N):
+    '''Efficient running mean (needs gap free data)'''
+    cumsum = numpy.cumsum(numpy.insert(x, 0, 0)) 
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
 
+def running_mean_gaps(x, y, window, minpoints=3, blur=0):
+    ''' Slow implementation of a running mean - but deals with data gaps'''
+    stat = np.zeros(len(x))
+    blurredstat = np.zeros(len(x))
+    scan = window / 2.
+    blurscan = blur / 2.
+    for point in np.arange(len(x)):
+        start = np.searchsorted(x,x[point]-scan)
+        end = np.searchsorted(x,x[point]+scan)
+        if end-start >= minpoints:
+            stat[point] = np.mean(y[start:end])
+        else:
+            stat[point] = 1000.
+    
+    if blur:
+        for point in np.arange(len(x)):
+            start = np.searchsorted(x,x[point]-blurscan)
+            end = np.searchsorted(x,x[point]+blurscan)
+            if end>start:
+                blurredstat[point] = np.min(stat[start:end])
+            else:
+                blurredstat[point] = 1000.
+    return stat, blurredstat
+
+def make_periodogram(tts_all,tds_all,ppset,fpset,windows,statdict):
+    '''
+    Turns a set of transit times and lightcurve stats into a periodogram
+    This is for 2d only - just period and true anomaly
+    
+    Parameters
+    ----------
+    tts_all 	: 	dict
+    				transit times from Nbody run
+    tds_all 	:	dict
+    				matching durations from Nbody run
+    ppset		:	list or array
+    				planet periods trialled
+    fpset		:	list or array
+    				true anomalies trialled
+    windows		:	list
+					Lightcurve windows used
+    statdict	:	dict
+    				Calculated lightcurve statistics (blurred or otherwise)
+    '''
+    periodogram = np.zeros([len(ppset),len(fpset)])
+    for ipp,pp in enumerate(ppset):
+        for ifp,fp in enumerate(fpset):
+            tts = tts_all[str(pp)[:6]][str(fp)[:6]]
+            tds = tds_all[str(pp)[:6]][str(fp)[:6]]
+            stat = 0
+            for t in range(len(tts)):
+                window = windows[np.argmin(np.abs(windows-tds[t]))]
+                timeindex = np.searchsorted(time,tts[t])
+                if timeindex < len(blurlcstat[window]):
+                    stat += blurlcstat[window][timeindex]-1
+            periodogram[ipp,ifp] = stat
+    return periodogram
+        
 def stack_metric(ts, fs):
     '''Return a metric for the given stacked light curves.'''
 
